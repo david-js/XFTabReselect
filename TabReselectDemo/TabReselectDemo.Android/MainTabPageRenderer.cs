70fe8b4f44d98fbdddef36c1663947ea8d465c72
Lester Sconyers <lester.sconyers@gmail.com>
2019-09-05 12:24:08 -0700

    Updated renderer for bottom tabs in Android

--- TabReselectDemo/TabReselectDemo.Android/MainTabPageRenderer.cs
+++ TabReselectDemo/TabReselectDemo.Android/MainTabPageRenderer.cs
@@ -19,13 +19,46 @@ using TabReselectDemo.Droid;
 [assembly: Xamarin.Forms.ExportRenderer(typeof(MainPage), typeof(MainTabPageRenderer))]
 namespace TabReselectDemo.Droid
 {
-    public class MainTabPageRenderer : TabbedPageRenderer, TabLayout.IOnTabSelectedListener
+    public class MainTabPageRenderer : TabbedPageRenderer, TabLayout.IOnTabSelectedListener, BottomNavigationView.IOnNavigationItemReselectedListener
     {
         public MainTabPageRenderer(Context context) : base(context)
         {
         }
 
         void TabLayout.IOnTabSelectedListener.OnTabReselected(TabLayout.Tab tab)
+        {
+            if (Element is MainPage)
+            {
+                var mainTabPage = Element as MainPage;
+                mainTabPage.NotifyTabReselected();
+            }
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<TabbedPage> e)
+        {
+            base.OnElementChanged(e);
+
+            if (e.OldElement == null && e.NewElement != null)
+            {
+                for (int i = 0; i <= this.ViewGroup.ChildCount - 1; i++)
+                {
+                    var childView = this.ViewGroup.GetChildAt(i);
+                    if (childView is ViewGroup viewGroup)
+                    {
+                        for (int j = 0; j <= viewGroup.ChildCount - 1; j++)
+                        {
+                            var childRelativeLayoutView = viewGroup.GetChildAt(j);
+                            if (childRelativeLayoutView is BottomNavigationView bottomNavigationView)
+                            {
+                                bottomNavigationView.SetOnNavigationItemReselectedListener(this);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        void BottomNavigationView.IOnNavigationItemReselectedListener.OnNavigationItemReselected(IMenuItem item)
         {
             if (Element is MainPage)
             {
